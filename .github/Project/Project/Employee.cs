9b4b0efd529635539bc36d8aa1b5ea3dd5de5393
JAYSHAH918 <59958579+JAYSHAH918@users.noreply.github.com>
2020-02-06 20:49:44 -0800

    Add files via upload

---
+++ .github/Project/Project/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class Employee
+    {
+         String Id;
+         String Name;
+         String DepartmentName;
+
+        public string GetId(String Id)
+        {
+            this.Id=Id;
+            return Id;
+        }
+        public string GetName(String Name)
+        {
+            this.Name = Name;
+            return Name;
+        }
+        public string GetDepartmentName(String DepartmentName)
+        {
+            this.DepartmentName = DepartmentName;
+            return DepartmentName;
+        }
+
+
+       /* internal void GetId(string a)
+        {
+            throw new NotImplementedException();
+            
+        }*/
+    }
+    
+}
